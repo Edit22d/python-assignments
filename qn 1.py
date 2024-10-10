ea3d7f4e4206bf35dcd63e1b3be9e686f6cd6969
Edit22d <nabwireedith18@gmail.com>
2024-10-10 00:42:00 -0700

    test 1

---
+++ qn 1.py
@@ -0,0 +1,6 @@
+#question 1(a)
+#create a python program that calculate the area of a triangle
+base = int(input('Enter the base of a triangle: \t'))
+height = int(input('bEnter the height of a triangle: \t'))
+area = (1/2) * base * height
+print(f'The area of a triangle of {base} and {height} is {area}')
---
+++ qn2.py
@@ -0,0 +1,7 @@
+#question 2(a)
+#usig a function, create a program that calculate the volume of a cylinder
+radius = int(input('Enter the radius of a cylinder:\t'))
+height = int(input('Enter the height of a cylinder:\t'))
+pie = float(input('Enter the pie of a cylinder:\t'))
+volume = (22/7) *radius**2*height
+print(f'The volume of a cylinder of {pie} and {radius**2} and {height} is { volume:}')
